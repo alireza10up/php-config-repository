4b44ce43726b644dff37ca6841bdc702828258ec
LOTOBM <toolsalireza10up@gmail.com>
2023-03-24 22:50:20 +0430

    FIX ALERT ADMIN

--- libs/helpers.php
+++ libs/helpers.php
@@ -2,7 +2,7 @@
 
 function diePage(string $msg = A_PROBLEM): string
 {
-    echo "<div style='width: 80%; color: #6e3030; padding: 1.2rem 1rem; margin: 1rem auto; background: #edbdbd; font-family: sans-serif; border-radius: 5px; border: solid #d1a7a7;'>{$msg}<br><a class='btn btn btn-primary' href= '" . BASE_URL . "'>رفتن به صفحه اصلی<a></div>";
+    echo "<div style='width: 80%; color: #6e3030; padding: 1.2rem 1rem; margin: 1rem auto; background: #edbdbd; border-radius: 5px; border: solid #d1a7a7;'>   {$msg} <hr>  <a class='btn btn btn-primary' href= '" . BASE_URL . "'>رفتن به صفحه اصلی<a></div>";
     die();
 }
 
